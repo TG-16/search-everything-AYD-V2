e2a4b0dd8ff8b0c285db643c314073a9c69f5b15
Ted <tgtewodros00@gmail.com>
2026-06-02 12:10:46 +0300

    basic folder structure done

---
+++ controllers/authController.js
@@ -0,0 +1,13 @@
+
+const register = (req, res) => {
+    // Implementation for user registration
+};
+
+const login = (req, res) => {
+    // Implementation for user login
+};
+
+module.exports = {
+    register,
+    login
+};
